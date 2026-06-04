0c7516add11161a34b8f4401e47eb080f8064516
Ronald Devia <ronaldstevenrios@gmail.com>
2026-06-03 21:46:01 -0500

    Agregando diseño del producto, csharp-app-blazer

---
+++ csharp-app-blazer/csharp-app-blazer/Models/ProductoItem.cs
@@ -0,0 +1,6 @@
+﻿// Models/ProductoItem.cs
+namespace csharp_app_blazer.Models
+{
+    public record ProductoItem(int Id, string Nombre, string Categoria, decimal Precio, int Stock);
+    public record MetricaItem(string Titulo, decimal Valor, string Subtitulo);
+}
